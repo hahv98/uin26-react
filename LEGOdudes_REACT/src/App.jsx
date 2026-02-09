c913be9ffd05b206f0dd97380afaaae762921137
hahv98 <henden.ahv@gmail.com>
2026-02-09 14:01:02 +0100

    whoopsido

--- LEGOdudes_REACT/src/App.jsx
+++ LEGOdudes_REACT/src/App.jsx
@@ -1,11 +1,15 @@
 import './style/lego.css'
 import { products } from './assets/legodudes'
+import { useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
 import CategoryTitle from './components/CategoryTitle.jsx'
 import Products from './components/Products.jsx'
 import Cart from './components/Cart.jsx'
-import { useEffect, useState } from 'react'
+import Layout from './components/Layout.jsx'
+
 function App() {
 
   const [isOpen, setIsOpen] = useState(false)
@@ -22,16 +26,28 @@ function App() {
     setTotalSum(total)
   },[cart])
 
+  function Page(){
+    return (
+      <div id="container">
+        <main>
+          <CategoryTitle />
+          <Products products={products} setCart={setCart} />
+        </main>
+      </div>
+    )
+  }
+
   return (
-    <div id="container">
-      <Header setIsOpen={setIsOpen} cartQuantity={cartQuantity}/>
-      <Nav />
-      <main>
-        <CategoryTitle />
-        <Products products={products} setCart={setCart} />
-      </main>
-      <Cart isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum}/>
-    </div>
+    <Layout setIsOpen={setIsOpen} cartQuantity={cartQuantity} isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum}>
+      <Routes>
+        <Route index element={<Page />} />
+        <Route path='city' element={<CategoryTitle title='City'/>} />
+        <Route path='ninjago' element={<CategoryTitle title='Ninjago'/>} />
+        <Route path='castles-and-knights' element={<CategoryTitle title='Castles & Knights'/>} />
+        <Route path='marine-and-pirates' element={<CategoryTitle title='Marine & Pirates'/>} />
+        <Route path='movie-characters' element={<CategoryTitle title='Movie characters'/>} />
+      </Routes>
+    </Layout>
   )
 }
 export default App
