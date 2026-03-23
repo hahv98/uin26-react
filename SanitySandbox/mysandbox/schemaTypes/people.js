8b9330b1178e4a9e89c2437b946978435fd0e6c4
hahv98 <henden.ahv@gmail.com>
2026-03-23 13:48:31 +0100

    sanity start

---
+++ SanitySandbox/mysandbox/schemaTypes/people.js
@@ -0,0 +1,9 @@
+const people = {
+    name: "people",
+    title: "Personer",
+    type: "document",
+    fields: [
+        { name: "fullname", title: "Fullt navn", type: "string" }
+    ]
+}
+export default people
